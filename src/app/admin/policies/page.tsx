0969f9fa1479856d4909a4766efe362f35eb550a
Zaeem <zaeemkhalid070@gmail.com>
2026-07-23 15:59:49 +0500

    feat: add switch UI component and implement admin policies page layout

--- src/app/admin/policies/page.tsx
+++ src/app/admin/policies/page.tsx
@@ -1,9 +1,14 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 import { AdminPoliciesPageContent } from '@/components/policies/admin-policies-page-content';
 
 export const metadata: Metadata = { title: 'Policies' };
 
 export default function AdminPoliciesPage() {
-  return <AdminPoliciesPageContent />;
+  return (
+    <Suspense>
+      <AdminPoliciesPageContent />
+    </Suspense>
+  );
 }
